57fd2b6ad7f1dc5d5adf72418d0465fbbb6c62b5
iamjoker021 <iamjoker021@gmail.com>
2024-07-22 16:57:21 +0530

    feat: Added UI to display Two Gameboard

--- src/index.js
+++ src/index.js
@@ -4,6 +4,13 @@ const { Player } = require('./player');
 const DOMHandler = () => {
     const players = []
 
+    const clearPlayground = () => {
+        const playground = document.querySelector('section.playground');
+        while (playground.firstChild) {
+            playground.removeChild(playground.firstChild);
+        }
+    }
+
     const homePage = () => {
         const container = document.querySelector('section.playground');
         
@@ -24,17 +31,49 @@ const DOMHandler = () => {
         form.appendChild(button);
     }
 
-    const playGround = () => {
+    const getGridDisplay = (player) => {
+        const gameboard = document.createElement('div');
+        gameboard.classList.add('gameboard');
 
+        const playerName = document.createElement('h3');
+        playerName.textContent = player.getName();
+        gameboard.appendChild(playerName);
+
+        const buttonGrid = document.createElement('div');
+        buttonGrid.classList.add('buttons');
+        gameboard.appendChild(buttonGrid);
+
+        player.getGameboard().getGrid().forEach((row, i) => {
+            row.forEach((val, j) => {
+                const button = document.createElement('button');
+                button.type='button';
+                if (val === 0) {
+                    button.textContent = 'X'
+                }
+                buttonGrid.appendChild(button);
+            })
+        })
+
+        return gameboard;
+    }
+
+    const displayPlaygroud = (players) => {
+        const container = document.querySelector('section.playground');
+        for (const player of players) {
+            container.appendChild(getGridDisplay(player));
+        }
     }
 
     homePage();
-
+// 
     document.querySelector('form.start-game').addEventListener('submit', (e) => {
         e.preventDefault();
         const playerName = e.target.querySelector('input[name="player"]').value;
         players[0] = Player(playerName);
         players[1] = Player('Computer');
+
+        clearPlayground();
+        displayPlaygroud(players);
     })
 
 }
